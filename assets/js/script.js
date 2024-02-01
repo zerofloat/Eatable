c0b1d184e0005480f14bfda1e504fdde86bc1f9e
zerofloat <133889261+zerofloat@users.noreply.github.com>
2024-02-01 19:58:15 +0000

    add additional API functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+    // DOM idea: mealType array values to be selected by dropdown/checkboxes
+    // idea: randomise which recipes are selected from the fetched data with pseudorandom number gen
+
+
 console.log('JS loaded');
 
 var queryURL;
@@ -10,34 +14,49 @@ var mealQueryString;
 var healthLabel;
 var recipe;
 var recipeImg;
+var recipeArray;
+var searchInput;
 
-function getRecipe() {
 
-    queryID = 'cf4c29ad';
-    // queryKey = '7a8da848a7f9a13b96f5cae45923af90';
-    // mealType array values to be selected by dropdown/checkboxes
+    //parameters for API call - present meal options in DOM
     mealType = ['Breakfast', 'Dinner', 'Lunch', 'Snack', 'Teatime'];
-    ingredient = 'bok choi';
+    
     mealQueryString = '&mealType=';
 
+    
+    $('#submit').on('click' , function () {
+        event.preventDefault();
+        searchInput = $('search').val().trim();
+        log(searchInput);
+        
 
+        // API strings
+        queryID = 'cf4c29ad';
+        // queryKey = '7a8da848a7f9a13b96f5cae45923af90'; commented out to prevent accidental rate-limiting
+        queryURL = `https://api.edamam.com/api/recipes/v2?type=public&q=${searchInput}&app_id=${queryID}&app_key=${queryKey}${mealQueryString}${mealType[2]}`;
 
-    queryURL = `https://api.edamam.com/api/recipes/v2?type=public&q=${ingredient}&app_id=${queryID}&app_key=${queryKey}${mealQueryString}${mealType[2]}`;
 
     fetch(queryURL)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            // randomise which recipes are selected from the response with pseudorandom number gen
+            recipeArray = [];
+            // receipeDiv
+            for (let i = 0; i < 1; i++) {
+                recipeEach = data.hits[i];                
+            }
 
+            recipe = data.hits[0]
             recipeImg = data.hits[0].recipe.images.LARGE.url;
-            // console.log(recipeImg);
+
             console.log(data.hits);
 
         }
         )
-}
+    }
+    )
+
 
 
 function getCocktail() {
